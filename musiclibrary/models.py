18e3ce9591585542fa8f9d843f0e0d9297da5858
Brian Lee <blee025432@gmail.com>
2025-03-13 00:27:46 -0400

    add detected cue file

--- musiclibrary/models.py
+++ musiclibrary/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.conf import settings
 import music_tag
+from mutagen.flac import FLACNoHeaderError
+from pathlib import Path
 
 # Create your models here.
 
@@ -14,24 +16,33 @@ class RawFiles(models.Model):
     raw_album = models.CharField(null=True, max_length=300)
     raw_artist = models.CharField(null=True, max_length=300)
     raw_tracktitle = models.CharField(null=True, max_length=300)
+    
+    # edge cases to capture for later analysis
+    failed_metadata_read = models.BooleanField(default=False)
+    detected_cue_file = models.BooleanField(default=False)
 
     def __str__(self):
-        return self.raw_tracktitle
+        return self.raw_filename.split("/")[-1]
 
     def grab_value(self, metadataitem):
-        length = len(metadataitem.values) 
-        assert length <= 1
-        if length == 1:
-            return metadataitem.values[0]
-        else:
-            return ""
+        
+        return " & ".join(metadataitem.values)
 
     def populate_raw_tag_fields(self):
-        tag_dict = music_tag.load_file(self.raw_filename)
-        self.raw_album = self.grab_value(tag_dict["album"])
-        self.raw_artist = self.grab_value(tag_dict["artist"])
-        self.raw_tracktitle = self.grab_value(tag_dict["tracktitle"])
+        try:
+            tag_dict = music_tag.load_file(self.raw_filename)
+            self.raw_album = self.grab_value(tag_dict["album"])
+            self.raw_artist = self.grab_value(tag_dict["artist"])
+            self.raw_tracktitle = self.grab_value(tag_dict["tracktitle"])
+        except (FLACNoHeaderError):
+            self.failed_metadata_read = True
+
+    def check_for_cue_file(self):
+        if len(list(Path(self.raw_filename).parent.absolute().glob("*.cue"))) > 0:
+            self.detected_cue_file = True
+
 
     def save(self, *args, **kwargs):
+        self.check_for_cue_file()
         self.populate_raw_tag_fields()
         super(RawFiles, self).save(*args, **kwargs)
